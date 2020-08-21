84f9dc6076326cade6440773918300f7a26d43d8
retarf <retarf@gmail.com>
2020-08-21 15:42:12 +0200

    Id's fields added to Car model.

--- project/cars/models.py
+++ project/cars/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 class Car(models.Model):
+    make_id = models.IntegerField()
     make = models.CharField(max_length=255)
+    model_id = models.IngegerField()
     model = models.CharField(max_length=255)
