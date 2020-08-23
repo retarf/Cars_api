3d83fdc38dd8f5b9d9100b1fe2e42aed3025ba66
retarf <retarf@gmail.com>
2020-08-23 07:39:51 +0200

    Rate model added.

--- project/rate/models.py
+++ project/rate/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+from cars.models import Car
+
+
+RateModel(models.Model):
+    car = models.ManyToMany(Car)
+    rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
