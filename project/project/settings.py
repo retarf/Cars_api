997d3069ea75930314c10fb81436b672f1cf2f13
retarf <retarf@gmail.com>
2020-08-23 22:56:55 +0200

    Settings changed.

--- project/project/settings.py
+++ project/project/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'testserver',
