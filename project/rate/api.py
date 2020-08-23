d941bc4fa90c39e3a93c8aa246898fd449b67610
retarf <retarf@gmail.com>
2020-08-23 15:03:29 +0200

    RateView post method save data.

--- project/rate/api.py
+++ project/rate/api.py
@@ -4,8 +4,14 @@ from rest_framework import status
 
 from .serializers import RateSerializer
 from .models import RateModel
+from cars.models import Car
 
 class RateView(APIView):
 
     def post(self, request, format=None):
-        print(request.data)
+        rate_serializer = RateSerializer(data=request.data)
+        if rate_serializer.is_valid():
+            rate_serializer.save()
+            return Response(rate_serializer.data, status.HTTP_201_CREATED)
+        else:
+            return Response(rate_serializer.errors, status.HTTP_403_FORBIDDEN)
--- project/rate/tests.py
+++ project/rate/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import RateModel
+
+
+class TestRateEndpoint(TestCase):
+
+    def test_rate_post_request__succeed(self):
+        car = {
+            'make': 'honda',
+            'model': 'CBX'
+        }
+        asserted_code = 201
+        endpoint = '/cars/'
+        c = APIClient()
+        response = c.post(endpoint, data=car)
+        self.assertEqual(asserted_code, response.status_code)
+
+        endpoint = '/rate/'
+        data = {
+            "car_id": 1,
+            "rate": 1
+        }
+        asserted_response_data = {
+            "car_id":1,
+            "rate":1
+            }
+        response = c.post(endpoint, data=data)
+        self.assertEqual(asserted_code, response.status_code)
+        self.assertEqual(asserted_response_data, response.json())
