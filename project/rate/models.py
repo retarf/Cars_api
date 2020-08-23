7f00f8f97f8b1768eab42b5991caaf5fa7839383
retarf <retarf@gmail.com>
2020-08-23 07:47:35 +0200

    Rate model fix.

--- project/rate/models.py
+++ project/rate/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from cars.models import Car
 
 
-RateModel(models.Model):
-    car = models.ManyToMany(Car)
+class RateModel(models.Model):
+    car = models.ManyToManyField(Car)
     rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
